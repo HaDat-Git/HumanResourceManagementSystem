83894326f6125304057731092a97061b949a0f87
Khanhayho1405 <nguyencongkhanh2004@gmail.com>
2025-06-14 15:26:33 +0700

    Congkhanh3

--- HumanResorce/obj/Debug/net8.0-windows/HumanResorce.AssemblyInfo.cs
+++ HumanResorce/obj/Debug/net8.0-windows/HumanResorce.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HumanResorce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+456ef3029c5a5e1c57452c06540f335e4029a430")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb48f0c748ed294d799111521f5cfb34040ccaa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("HumanResorce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HumanResorce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
